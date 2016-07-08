a7ed1e283f4d63d6cb903b1d3ceb1d5a28c16930
eddyfadey <edikfadeev@icloud.com>
2016-07-08 17:37:28 +0200

    done

---
+++ eduard/src/main/java/hw01/BinaryCalc.java
@@ -0,0 +1,15 @@
+package hw01;
+
+public class BinaryCalc {
+    public static void main(String[] args) {
+        int i = 1010001;
+        double i1 = 1 * Math.pow(2, 6);
+        double i2 = 0 * Math.pow(2, 5);
+        double i3 = 1 * Math.pow(2, 4);
+        double i4 = 0 * Math.pow(2, 3);
+        double i5 = 0 * Math.pow(2, 2);
+        double i6 = 0 * Math.pow(2, 1);
+        double i7 = 1 * Math.pow(2, 0);
+        System.out.println(i1 + i2 + i3 + i4 + i5 + i6 + i7);
+    }
+}
