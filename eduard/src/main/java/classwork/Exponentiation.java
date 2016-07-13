c0d054d86333133ababea649a5d5bf5506124133
eddyfadey <edikfadeev@icloud.com>
2016-07-13 17:09:42 +0200

    done

---
+++ eduard/src/main/java/classwork/Exponentiation.java
@@ -0,0 +1,27 @@
+package classwork;
+
+/**
+ * Created by edikf on 13.07.2016.
+ */
+public class Exponentiation {
+    public static void main(String[] args) {
+
+       // int i = Math.pow()
+
+        System.out.println(exponent(2, 3));
+        System.out.println(exponent(2, 6));
+
+        int k = exponent(2, exponent(2, 3));
+
+        System.out.println(k);
+    }
+
+    public static int exponent (int num, int exp) {
+        int res = num;
+
+        for (int i = 1; i < exp; i++) {
+            res = res * num;
+        }
+        return res;
+    }
+}
---
+++ eduard/src/main/java/classwork/Methods.java
@@ -0,0 +1,16 @@
+package classwork;
+
+public class Methods {
+    public static void main(String[] args) {
+        System.out.print(getSomeInt());
+        System.out.println(getSomeString());
+
+    }
+
+    public static int getSomeInt() {
+        return 5;
+    }
+    public static String getSomeString () {
+        return "Kotov";
+    }
+}
