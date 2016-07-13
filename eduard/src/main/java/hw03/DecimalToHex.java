4b982192cf1c1536ba0a71f5b2e608a47662e30c
eddyfadey <edikfadeev@icloud.com>
2016-07-13 15:11:42 +0200

    done

--- eduard/src/main/java/hw03/DecimalToHex.java
+++ eduard/src/main/java/hw03/DecimalToHex.java
@@ -5,7 +5,6 @@ public class DecimalToHex {
         int i;
         String res = "";
         char dig[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
     }
 
 }
--- eduard/src/main/java/hw04/Doska.java
+++ eduard/src/main/java/hw04/Doska.java
@@ -1,4 +1,24 @@
 package hw04;
 
+import java.util.Scanner;
+
 public class Doska {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Задайте размер доски: ");
+        if (scan.hasNextInt()) {
+            int h = scan.nextInt();
+            int w = scan.nextInt();
+            for (int i = 0; i < h; i++) {
+                for (int k = 0; k < w; k++)
+                    if ((i + k) % 2 == 0) {
+                        System.out.print("Ж Ж Ж Ж ");
+                    } else
+                        System.out.print("- - - - ");
+                System.out.println();
+            }
+        }
+
+    }
+
 }
