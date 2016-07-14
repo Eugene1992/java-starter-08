fb2a04a3fe934fecfabc0f6e8a71b0925282069c
Tarik2121 <khorunziytaras@gmail.com>
2016-07-14 12:20:32 +0300

    hw3

--- taras/src/main/java/hw3/task4.java
+++ taras/src/main/java/hw3/task4.java
@@ -3,18 +3,22 @@ package hw3;
 /**
  * Created by Тарас on 12.07.2016.
  */
-public class task4 {
+public class Task4 {
     public static void main(String[] args) {
         //Завдання №4
-        int start, end, step, r;
+        int start, end, step;
         start = 2;
         step = 2;
-        r = 1;
         end = 20;
+        geometricProgression(start, step, end);
+    }
+    static int geometricProgression(int start, int step, int end){
         String res = "";
+        int r = 1;
         for (int i = start; r < end; i = i * step) {
             System.out.println(i + " ");
             r++;
         }
+        return r;
         }
     }
--- taras/src/main/java/hw4/hw4_2.java
+++ taras/src/main/java/hw4/hw4_2.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 /**
  * Created by Тарас on 13.07.2016.
  */
-public class hw4_2 {
+public class Hw4_2 {
     public static void main(String[] args) {
         //Завдання 2
         Scanner scan = new Scanner(System.in);
-        int a, b, fact, c, r;
-        a = 1;
+        int b;
         System.out.println("Введіть числo");
         b = scan.nextInt();
+        factorial(b);
+    }
+        static int factorial(int b){
+        int a, fact, c, r;
+        a = 1;
         fact = 0;
         c = b;
         r = 1;
         String res = "";
         for (int i = b; r < c; i = a * b) {
+
             fact = a * b;
             b = fact;
             a++;;
             r++;
         }
-        System.out.println(fact);
+            System.out.println(fact);
+            return r;
     }
 }
 
